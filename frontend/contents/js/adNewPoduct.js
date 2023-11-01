066451ac1d963bfc38ea70e7d32b308771a29569
alireza talebizadeh <alireza.talebizadeh78@gmail.com>
2023-11-01 17:48:53 +0330

    coding to show all products and delete modal

--- frontend/contents/js/adNewPoduct.js
+++ frontend/contents/js/adNewPoduct.js
@@ -7,28 +7,114 @@ let priceProductElem = document.querySelector("#priceProduct"),
   colorsProductElem = document.querySelector("#colorsProduct"),
   addProductBtn = document.querySelector(".add_product_submit");
 
+//! clear all of inputs
+function clearInputs() {
+  priceProductElem.value = "";
+  nameProductElem.value = "";
+  countProductElem.value = "";
+  addressProductElem.value = "";
+  popularityProductElem.value = "";
+  saleProductElem.value = "";
+  colorsProductElem.value = "";
+}
+
+//! validation all inputs
+function validationInputs() {
+  if (
+    nameProductElem.value &&
+    priceProductElem.value &&
+    countProductElem.value &&
+    addressProductElem.value &&
+    popularityProductElem.value &&
+    saleProductElem.value &&
+    colorsProductElem.value
+  ) {
+    return true;
+  }
+  return false;
+}
+
+//! create newProduct
 addProductBtn.addEventListener("click", (e) => {
   e.preventDefault();
+  if (validationInputs()) {
+
+    let infoNewProduct = {
+      title: nameProductElem.value,
+      price: Number(priceProductElem.value),
+      count: Number(countProductElem.value),
+      src: addressProductElem.value,
+      popularity: Number(popularityProductElem.value),
+      sale: Number(saleProductElem.value),
+      colors: Number(colorsProductElem.value),
+    };
 
 
-  let infoNewProduct = {
-    title: nameProductElem.value,
-    price: Number(priceProductElem.value),
-    count: Number(countProductElem.value),
-    src: addressProductElem.value,
-    popularity: Number(popularityProductElem.value),
-    sale: Number(saleProductElem.value),
-    colors: Number(colorsProductElem.value),
-  };
-  console.log(infoNewProduct);
+    
+    fetch("http://localhost:3000/api/products/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(infoNewProduct),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result);
+        let mainProductElem = document.querySelector(".main");
 
-  fetch("http://localhost:3000/api/products/", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(infoNewProduct),
-  })
-    .then((res) => res.json())
-    .then((res) => console.log(res));
+        fetch("http://localhost:3000/api/products/")
+          .then((res) => res.json())
+          .then((allProducts) => {
+            if (allProducts.length) {
+          //     mainProductElem.insertAdjacentHTML(
+          //       "beforeend",
+          //       `<table class="product_table">
+          //      <thead>
+          //        <tr class="product_table_heading_tr">
+          //          <th>عکس</th>
+          //          <th>اسم</th>
+          //          <th>قیمت</th>
+          //          <th>موجودی</th>
+          //        </tr> 
+          //     </thead>
+          //     <tbody class='product_content'></tbody>
+          //  </table>`
+          //     );
+
+              const productsContent =
+                document.querySelector(".product_content");
+
+              allProducts.forEach((product) => {
+                productsContent.insertAdjacentHTML(
+                  "beforeend",
+                  `
+             <tr class="product_table_tr">
+               <td><img src="${product.src}" class="product_table_img" alt="imp_product"></tی>
+               <td>${product.title}</td>
+               <td>${product.price}</td>
+               <td>${product.count}</td>
+               <td>
+                  <button class="products_table_button">جزيیات</button>
+                  <button class="products_table_button">حذف</button>
+                  <button class="products_table_button">ویرایش</button>
+               </td>
+            </tr>`
+                );
+              });
+            } else {
+              mainProductElem.insertAdjacentHTML(
+                "beforeend",
+                `<div class="cms_empty_error">
+              هیچ محصولی یافت نشد
+           </div>`
+              );
+            }
+          });
+
+        clearInputs();
+      });
+  } else {
+    alert("please insert valid data");
+  }
 });
--- frontend/contents/js/products.js
+++ frontend/contents/js/products.js
@@ -1,15 +1,36 @@
-window.addEventListener("load", () => {
-  let mainProduct = document.querySelector(".product_table");
-  let mainProductElem = document.querySelector(".products_main");
+let deleteModal = document.querySelector("#delete_modal");
+let unAcceptDeleteBtn = document.querySelector("#unAcceptDelete");
+let confirmDeleteBtn = document.querySelector("#confirmDelete");
+let mainProductElem = document.querySelector(".main");
+let editModal = document.querySelector("#edit_Modal");
+let globalProductID = null;
 
+//! show edit modal
+function showDeleteModal(productID) {
+  globalProductID = productID;
+  deleteModal.classList.add("active");
+}
+
+//!  show edit modal
+function showEditModal(productID) {
+  globalProductID = productID;
+  editModal.classList.add("active");
+}
+
+//! hidden edit modal
+function hiddenEditModal() {
+  editModal.classList.remove("active");
+}
+
+//! get all products
+function getAllProduct() {
   fetch("http://localhost:3000/api/products/")
     .then((res) => res.json())
     .then((allProducts) => {
-
       if (allProducts.length) {
-          mainProductElem.insertAdjacentHTML(
-            "beforeend",
-            `<table class="product_table">
+        mainProductElem.insertAdjacentHTML(
+          "beforeend",
+          `<table class="product_table">
                <thead>
                  <tr class="product_table_heading_tr">
                    <th>عکس</th>
@@ -20,26 +41,27 @@ window.addEventListener("load", () => {
               </thead>
               <tbody class='product_content'></tbody>
            </table>`
-          );
+        );
 
-          const productsContent = document.querySelector(".product_content");
+        const productsContent = document.querySelector(".product_content");
 
-          allProducts.forEach((product) => {
-            productsContent.insertAdjacentHTML("beforeend", `
-             <tr class="product_table_tr">
+        productsContent.innerHTML = "";
+        allProducts.forEach((product) => {
+          productsContent.insertAdjacentHTML(
+            "beforeend",
+            `<tr class="product_table_tr">
                <td><img src="${product.src}" class="product_table_img" alt="imp_product"></tی>
                <td>${product.title}</td>
                <td>${product.price}</td>
                <td>${product.count}</td>
                <td>
                   <button class="products_table_button">جزيیات</button>
-                  <button class="products_table_button">حذف</button>
-                  <button class="products_table_button">ویرایش</button>
+                  <button class="products_table_button" onclick="showDeleteModal(${product.id})">حذف</button>
+                  <button class="products_table_button" onclick='showEditModal(${product.id})'>ویرایش</button>
                </td>
-            </tr>`)
-          });
-       
-        
+            </tr>`
+          );
+        });
       } else {
         mainProductElem.insertAdjacentHTML(
           "beforeend",
@@ -49,4 +71,38 @@ window.addEventListener("load", () => {
         );
       }
     });
+}
+
+//!hidden modal's delete
+function hiddenDeleteModal() {
+  deleteModal.classList.remove("active");
+}
+
+//! delete product
+confirmDeleteBtn.addEventListener("click", () => {
+  fetch(`http://localhost:3000/api/products/${globalProductID}`, {
+    method: "DELETE",
+  }).then((res) => {
+    console.log(res);
+    hiddenDeleteModal();
+    getAllProduct();
+  });
+});
+
+window.addEventListener("load", getAllProduct);
+unAcceptDeleteBtn.addEventListener("click", hiddenDeleteModal);
+
+//!hidden modal's delete to click mouse
+window.addEventListener("click", (event) => {
+  if (event.target.id === "delete_modal") {
+    hiddenDeleteModal();
+  }
+});
+
+//! hidden modals's delete to click in Scape button on keyboard
+window.addEventListener("keydown", (event) => {
+  console.log(event);
+  if (event.key === "Escape") {
+    hiddenDeleteModal();
+  }
 });
